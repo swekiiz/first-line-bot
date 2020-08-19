a95e6dba618385affb0f3d0abb3b78445509a5b7
Thanadol sinananvanich <40865904+swekiiz@users.noreply.github.com>
2020-08-20 02:04:32 +0700

    Update Config.py

--- project/Config.py
+++ project/Config.py
@@ -1,4 +1,7 @@
-channel_id = '1654665135'
-channel_secrat = 'c8b8b0517f3772dda01cf1bbb8a98a56'
-basic_id = '@866ouetm'
-channel_access_token = '/+DNNOcosWVxQCOeNbZwD70mzMuizLquxZZfcI5orsxzlw/7l7/xtuwOgga4VemvoGH0eWEsvczxvD4A7h338oA/urwe5KEuEGildFEp6l8A+Zzfj3XO+pgCWIbs8JvM2BoTUduZCWGG0ZozxbpCqAdB04t89/1O/w1cDnyilFU='
+channel_id = ''
+channel_secrat = ''
+basic_id = ''
+channel_access_token = ''
+
+
+# cannot public
